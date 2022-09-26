35c7df52f0849b18273b1c41e9c39ab45060e4e7
fursov231 <fursov231@hotmail.com>
2022-09-26 16:54:46 +0300

    feat: add request controller

--- gateway/src/main/java/ru/practicum/ewm/request/controller/RequestController.java
+++ gateway/src/main/java/ru/practicum/ewm/request/controller/RequestController.java
@@ -1,4 +1,27 @@
 package ru.practicum.ewm.request.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.ewm.request.client.RequestPrivateClient;
+
+@RestController
+@RequiredArgsConstructor
 public class RequestController {
+    private final RequestPrivateClient privateClient;
+
+    @GetMapping("/users/{userId}/requests")
+    public ResponseEntity<Object> getAllRequests(@RequestHeader("X-Sharer-User-Id") long ownerId,  @PathVariable long userId) {
+        return privateClient.getAllRequests(ownerId, userId);
+    }
+
+    @PostMapping("/users/{userId}/requests")
+    public ResponseEntity<Object> addRequest(@RequestHeader("X-Sharer-User-Id") long ownerId,  @PathVariable long userId, @RequestParam long eventId) {
+        return privateClient.addRequest(ownerId, userId, eventId);
+    }
+
+    @PatchMapping("/users/{userId}/requests/{requestId}/cancel")
+    public ResponseEntity<Object> cancelRequest(@RequestHeader("X-Sharer-User-Id") long ownerId,  @PathVariable long userId, @PathVariable long requestId) {
+        return privateClient.cancelRequest(ownerId, userId, requestId);
+    }
 }
